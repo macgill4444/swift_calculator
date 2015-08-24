c65761f8efcb396c8bca02990eafbfc30403854a
Macgill Davis <macgill4444@gmail.com>
2015-08-24 13:58:25 -0400

    almost done with first calculator app yA

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+    @IBOutlet weak var display: UILabel!
+
+    var userIsInTheMiddleOfTypingANumber = false
+   
+    var brain = CalculatorBrain()
+    
+    //make sure with action you dont choose anyobject
+    @IBAction func appendDigit(sender: UIButton) {
+        //let same as var but constant, make sure you put a type
+        let digit = sender.currentTitle!
+        //when using ! it unwraps optional and when that value is not set then it crashes
+        
+        if userIsInTheMiddleOfTypingANumber{
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+ 
+    @IBAction func operate(sender: UIButton) {
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
     }
-
-
+    
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            //newValue is a magic variable
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
+    
+    @IBAction func clearCalc() {
+        userIsInTheMiddleOfTypingANumber = false
+        displayValue = 0
+        brain.clear()
+    }
+    
 }
 
